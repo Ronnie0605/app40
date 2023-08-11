1d47cab7d5bf2d89f631adc7c23706fa9a13d9da
Ronnie0605 <122019162+Ronnie0605@users.noreply.github.com>
2023-08-11 13:33:34 +0530

    5 files added

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css'],
+})
+export class HerosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
