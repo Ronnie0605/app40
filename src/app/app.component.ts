9673d94d758994affff80c28c6c468a4d6feb97f
Ronnie0605 <122019162+Ronnie0605@users.noreply.github.com>
2023-08-11 14:42:11 +0530

    1 file modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HEROES } from './mock-heroes';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[HeroesComponent,CommonModule],
+  imports: [HeroesComponent, CommonModule, HeroDetailComponent],
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
